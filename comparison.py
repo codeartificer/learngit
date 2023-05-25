d27adcf60754aec652204116c93431c62f492dd4
codeartificer <44143744+codeartificer@users.noreply.github.com>
2023-05-25 06:16:44 -0700

    comaprison in python

---
+++ comparison.py
@@ -0,0 +1,7 @@
+number = int(input("enter your number :"))
+if number > 0 :
+ print("the number is greater tha 0")
+elif number < 0 :
+  print("the number is less than 0")
+else :
+ print("the number is 0")
